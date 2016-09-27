b7da49099c9c5af87a8fed2d3325508882e39efa
Rick Carlino <rick.carlino@gmail.com>
2016-09-26 22:58:31 -0500

    Add entry

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-import "./compiler";
-import "./default_instructions";
-import "./instruction";
-import "./instruction_set";
-import "./vm";
+import * as compiler1 from "./compiler";
+import * as defins1 from "./default_instructions";
+import * as ins1 from "./instruction";
+import * as ins_set1 from "./instruction_set";
+import * as VM1 from "./vm";
 
+export let { Compiler} = compiler1;
+export let { DefaultInstructions } = defins1;
+export let { Instruction } = ins1;
+export let { InstructionSet } = ins_set1;
+export let { VM } = VM1;
