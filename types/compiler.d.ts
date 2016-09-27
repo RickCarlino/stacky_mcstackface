da2e395e816811f8c6ab05f490b266ca41c61d22
Rick Carlino <rick.carlino@gmail.com>
2016-09-26 22:44:15 -0500

    Update package.json

--- types/compiler.d.ts
+++
@@ -1,4 +0,0 @@
-import { InstructionSet } from "./instruction_set";
-/** Generate a compiler function that can convert text chunks into arrays of
- * op codes. */
-export declare let Compiler: (i?: InstructionSet) => (input: string) => number[];
--- types/spec/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-import { VM } from "../vm";
-export declare function run(code: string, vm?: VM): VM;
-export declare function dump(vm: VM): void;
-export declare function inspectFirst(x: number, vm: VM): void;
